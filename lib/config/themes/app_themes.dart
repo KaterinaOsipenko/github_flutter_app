4ba52f8b531eb20e9963356889dddef73537df12
Kateryna Osypenko <kate.osipenko30@gmail.com>
2023-05-29 18:46:42 +0300

    create theme

---
+++ lib/config/themes/app_themes.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+abstract class AppTheme {
+  static ThemeData get light {
+    return ThemeData(
+      colorScheme: const ColorScheme.light(
+        primary: Color.fromRGBO(20, 99, 245, 1),
+        onPrimary: Color.fromRGBO(33, 24, 20, 1),
+        secondary: Color.fromRGBO(239, 227, 255, 1),
+        background: Color.fromRGBO(249, 249, 249, 1),
+        surface: Color.fromRGBO(242, 242, 242, 1),
+        onSurface: Color.fromRGBO(191, 191, 191, 1),
+      ),
+      textTheme: TextTheme(
+        displayMedium: GoogleFonts.raleway(
+          textStyle: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+            letterSpacing: 0,
+          ),
+        ),
+        bodyMedium: GoogleFonts.raleway(
+          textStyle: const TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:github_search_app/config/router/app_route.gr.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ProviderScope(
+    child: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
   final appRouter = AppRouter();
+
+  MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
       routerDelegate: appRouter.delegate(),
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      debugShowCheckedModeBanner: false,
+      routeInformationParser: appRouter.defaultRouteParser(),
     );
   }
 }
